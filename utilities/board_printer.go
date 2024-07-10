7c174259a7cf838e841236346523cbcd5f577611
andyosyndoh <andyovvo8@gmail.com>
2024-07-10 17:04:34 +0300

    fix : naming of function

--- utilities/board_printer.go
+++ utilities/board_printer.go
@@ -2,8 +2,8 @@ package utilities
 
 import "fmt"
 
-// PrintBoard prints the final square.
-func PrintBoard(board [][]string) {
+// Print prints the final square.
+func Print(board [][]string) {
 	for _, row := range board {
 		for _, cell := range row {
 			fmt.Print(cell)
